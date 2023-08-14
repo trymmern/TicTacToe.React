d725dea4e86744bf30d3b4d3e7fe29c343cc0afe
Trym Todalshaug <trymrt@gmail.com>
2023-08-14 14:35:40 +0200

    move Colors.sass to /theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,19 @@
-import Avatar from "react-avatar"
-import "../styles/Footer.sass"
+import Avatar from "react-avatar";
+import { UncontrolledTooltip } from "reactstrap";
+import "../styles/Footer.sass";
 
 export default function Footer({user, users}: {user: string, users: string[]}) {
     return (
         <div className="footer-container">
             {users.filter((u) => u !== user).map((u) => {
                 return (
-                    <div key={u}>
+                    <div>
                         <span key={u} id={u} className="user-info">
-                            <Avatar name={u} size="40" round="20px"></Avatar>
+                            <Avatar name={u} size="40" round="20px" />
                         </span>
+                        <UncontrolledTooltip placement="bottom" target={u}>
+                            {u}
+                        </UncontrolledTooltip>
                     </div>
                 )
             })}
